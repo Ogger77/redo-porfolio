4b3c4967c2d8be57d4f6d4f1cd3b257683740f3c
ogger77 <vud@kean.edu>
2021-06-17 13:22:29 -0400

    added skill tree

--- js/main.js
+++ js/main.js
@@ -52,3 +52,12 @@ $(function () {
   }
   navbarFixed();
 });
+$("body").scrollspy({ target: "#navbarNav" });
+
+var offset = 200;
+
+$(".navbar li a").click(function (event) {
+  event.preventDefault();
+  $($(this).attr("href"))[0].scrollIntoView();
+  scrollBy(0, -offset);
+});
