3731ad6cbc836054e145208f6ba18f5159ced957
ogger77 <vud@kean.edu>
2021-06-22 00:57:34 -0400

    build ready

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
 $(function () {
-  let $btns = $(".project-area .button-group button");
-
-  $(".project-area .button-group #btn1").trigger("click");
-
-  $btns.on("click", function (e) {
-    $(".project-area .button-group button").removeClass("active");
-    e.target.classList.add("active");
-
-    let selector = $(e.target).attr("data-filter");
-    $(".project-area .grid").isotope({
-      filter: selector,
-    });
-
-    return false;
-  });
-
-  $(".project-area .grid .test-popup").magnificPopup({
-    type: "image",
-    gallery: { enabled: true },
-  });
-
-  // Carousel
-  $(".site-main .about-area .owl-carousel").owlCarousel({
-    loop: true,
-    autoplay: true,
-    dots: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      544: {
-        items: 2,
-      },
-    },
-  });
-
   //sticky navigation menu
   let nav_offset_top = $(".header_area").height() + 50;
 
@@ -65,20 +29,20 @@ $(".navbar li a").click(function (event) {
 });
 
 // scroll animation
-const observer = new IntersectionObserver(entries => {
+const observer = new IntersectionObserver((entries) => {
   // Loop over the entries
-  entries.forEach(entry => {
-    const separator = entry.target.querySelector('.separator--line');
+  entries.forEach((entry) => {
+    const separator = entry.target.querySelector(".separator--line");
     // If the element is visible
     if (entry.isIntersecting) {
       // Add the animation class
-      separator.classList.add('separator-animate');
+      separator.classList.add("separator-animate");
       return;
     }
 
     // remove if not intersecting
-    separator.classList.remove('separator-animate');
+    separator.classList.remove("separator-animate");
   });
 });
 
-observer.observe(document.querySelector('.separator-wrapper'));
+observer.observe(document.querySelector(".separator-wrapper"));
